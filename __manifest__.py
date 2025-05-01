5611c945fd152bbd220f09dd1e6458614178fd7f
Zain Javed <42720762+Engr-zainjaved@users.noreply.github.com>
2025-05-01 12:06:03 +0500

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Auth_bypass',
+    'version': '1.0',
+    'author': 'Click2Deploy',
+    'category': 'Custom',
+    'summary': 'Authenticate users via session ID in URL',
+    'depends': ['web'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
